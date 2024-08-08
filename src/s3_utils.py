15df58210da52390d99a7abf23f4b577fa181502
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-08-08 11:24:32 +0200

    Create s3_utils.py

---
+++ src/s3_utils.py
@@ -0,0 +1,11 @@
+import s3fs
+import streamlit as st
+
+def get_s3_fs():
+    return s3fs.S3FileSystem(
+        key=st.secrets["aws"]["AWS_ACCESS_KEY_ID"],
+        secret=st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"],
+        client_kwargs={
+            'region_name': st.secrets["aws"]["AWS_DEFAULT_REGION"]
+        }
+    )
