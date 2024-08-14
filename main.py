3a7813e491cb309091a5b4b72d2f6ed6cd36f74c
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-08-14 12:56:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from src.data_processor import process_orders, create_dataframe, save_to_csv
 from src.fulfillment_costs import calculate_shipping_costs, load_fulfillment_costs, save_fulfillment_costs
 from src.transaction_costs import load_transaction_costs, save_transaction_costs
 from src.marketing_costs import load_marketing_costs, save_marketing_costs
+from src.material_costs import load_material_costs, save_material_costs, manage_material_costs
 
 
 # Configure logging
